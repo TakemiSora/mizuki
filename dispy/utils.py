1ed652da2497321f442d569fea1a810fbfc144b3
TakemiSora <sorakaritakemi@gmail.com>
2026-05-30 12:51:05 +0000

    Forgot to intialize missingsentinel

--- dispy/utils.py
+++ dispy/utils.py
@@ -8,7 +8,7 @@ class Missing:
     def __bool__(self) -> bool:
         return False
 
-_MISSING: Any = Missing
+_MISSING: Any = Missing()
 
 def sint(txt: str | None) -> int | None:
     return int(txt) if txt else None
