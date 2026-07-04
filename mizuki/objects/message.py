9ac399fc8da4cb65d5c6cab171e5ec24a0b20f3f
TakemiSora <sorakaritakemi@gmail.com>
2026-07-04 11:35:06 +0530

    [FEAT] Add methods to Message object

--- mizuki/objects/message.py
+++ mizuki/objects/message.py
@@ -39,6 +39,9 @@ from mizuki.payloads.message import (
 
 if TYPE_CHECKING:
     from mizuki.state import ConnectionState
+    from mizuki.file import File
+
+    from mizuki.enums.message import ReactionType
 
 __all__ = (
     "Attachment",
@@ -383,6 +386,341 @@ class Message(PartialMessage):
         self.poll = scls(Poll, data.get("poll"))
         self.shared_client_theme = scls(
             SharedClientTheme, data.get("shared_client_theme")
+        )
+
+    async def reply(
+        self,
+        content: str = _MISSING,
+        *,
+        tts: bool = _MISSING,
+        embeds: list[Embed] = _MISSING,
+        allowed_mentions: AllowedMentions = _MISSING,
+        files: list[File] = _MISSING,
+        sticker_ids: list[int] = _MISSING,
+        flags: MessageFlags = _MISSING,
+    ) -> Message:
+        """
+        Creates a new reply to a message in the specified channel.
+
+        .. note::
+
+            At least one of, ``content``, ``embeds``, ``sticker_ids``, ``files`` must be provided.
+
+        Parameters
+        ----------
+        content : :class:`str`
+            The content of the message.
+
+        tts : :class:`bool`
+            Whether TTS is enabled for the message.
+
+        embeds : list[:class:`Embed <mizuki.objects.embed.Embed>`]
+            The list of embeds to send along the message.
+
+        allowed_mentions : :class:`AllowedMentions <mizuki.objects.message.AllowedMentions>`
+            The AllowedMentions object that dictates whether user, role or everyone pings are enabled.
+
+        files : list[:class:`File <mizuki.file.File>`]
+            The files to upload with the message.
+
+        sticker_ids : list[:class:`int`]
+            The Guild Stickers to send with the message. Max 3.
+
+        flags : :class:`MessageFlags <mizuki.flags.MessageFlags>`
+            The MessageFlags of the new message.
+
+        Raises
+        ------
+        :class:`NotFound`
+            The channel you tried to send to doesn't exist or the message you replied to doesn't exist.
+
+        :class:`Forbidden`
+            You are not allowed to send the message. You may be missing a specific permission.
+
+        :class:`HTTPException`
+            A HTTP error occurred.
+        """
+        return await self._state.managers.messages.reply(
+            self.channel_id,
+            self.id,
+            content=content,
+            tts=tts,
+            embeds=embeds,
+            allowed_mentions=allowed_mentions,
+            files=files,
+            sticker_ids=sticker_ids,
+            flags=flags,
+        )
+
+    async def forward(
+        self,
+        target_channel_id: int,
+    ) -> Message:
+        """
+        Forwards a message to a channel.
+
+        Parameters
+        ----------
+        target_channel_id : :class:`int`
+            The ID of the target Channel.
+
+        Raises
+        ------
+        :class:`NotFound`
+            The channel you tried to send to doesn't exist or the message you tried to forward doesn't exist.
+
+        :class:`Forbidden`
+            You are not allowed to send the message. You may be missing a specific permission.
+
+        :class:`HTTPException`
+            A HTTP error occurred.
+        """
+        return await self._state.managers.messages.forward(
+            target_channel_id,
+            message_id=self.id,
+            channel_id=self.channel_id,
+        )
+
+    async def crosspost(self) -> Message:
+        """
+        Crossposts a message from an Announcement Channel to all following channels.
+
+        This requires :attr:`SEND_MESSAGES <mizuki.objects.permissions.Permissions.SEND_MESSAGES>` for your own messages, and
+        :attr:`MANAGE_MESSAGES <mizuki.objects.permissions.Permissions.MANAGE_MESSAGES>` when crossposting messages sent by others.
+
+        Raises
+        ------
+        :class:`NotFound`
+            The channel you tried to crosspost from doesn't exist or the message you tried to crosspost doesn't exist.
+
+        :class:`Forbidden`
+            You are not allowed to crosspost the message. You may be missing a specific permission.
+
+        :class:`HTTPException`
+            A HTTP error occurred.
+        """
+        return await self._state.managers.messages.crosspost(self.channel_id, self.id)
+
+    async def react(
+        self,
+        *,
+        emoji_id: int = _MISSING,
+        emoji_name: str,
+    ) -> None:
+        """
+        Adds a reaction to a message.
+
+        Requires :attr:`READ_MESSAGE_HISTORY <mizuki.objects.permissions.Permissions.READ_MESSAGE_HISTORY>`. Also requires :attr:`ADD_REACTIONS <mizuki.objects.permissions.Permissions.ADD_REACTIONS>` if no one has reacted with this emoji.
+
+        Parameters
+        ----------
+        emoji_id : :class:`int`, optional
+            The ID of the custom emoji. Omit when reacting with a unicode emoji.
+
+        emoji_name : :class:`str`
+            The unicode emoji or the name of the custom emoji.
+
+        Raises
+        ------
+        :class:`NotFound`
+            The message you tried to react to or the emoji you tried to react with wasn't found.
+
+        :class:`Forbidden`
+            You are not allowed to react/see the message. You may be missing a specific permission.
+
+        :class:`HTTPException`
+            A HTTP error occurred.
+        """
+        return await self._state.managers.messages.react(
+            channel_id=self.channel_id,
+            message_id=self.id,
+            emoji_id=emoji_id,
+            emoji_name=emoji_name,
+        )
+
+    async def remove_reaction(
+        self,
+        *,
+        user_id: int = _MISSING,
+        emoji_id: int = _MISSING,
+        emoji_name: str,
+    ) -> None:
+        """
+        Removes your reaction from a message.
+
+        Parameters
+        ----------
+        user_id : :class:`int`, optional
+            The ID of the target user. Defaults to client user.
+
+        emoji_id : :class:`int`, optional
+            The ID of the custom emoji. Omit when reacting with a unicode emoji.
+
+        emoji_name : :class:`str`
+            The unicode emoji or the name of the custom emoji.
+
+        Raises
+        ------
+        :class:`NotFound`
+            The message you tried to remove reaction from or the emoji you tried to remove reaction of wasn't found.
+
+        :class:`Forbidden`
+            You are not allowed to remove reaction/see the message. You may be missing a specific permission.
+
+        :class:`HTTPException`
+            A HTTP error occurred.
+        """
+        return await self._state.managers.messages.remove_reaction(
+            channel_id=self.channel_id,
+            message_id=self.id,
+            user_id=user_id,
+            emoji_id=emoji_id,
+            emoji_name=emoji_name,
+        )
+
+    async def remove_all_reactions(self) -> None:
+        """
+        Removes all reactions from a message. This method requires :attr:`MANAGE_MESSAGES <mizuki.objects.permissions.Permissions.MANAGE_MESSAGES>`.
+
+        Raises
+        ------
+        :class:`NotFound`
+            The message you tried to do this action on does not exist.
+
+        :class:`Forbidden`
+            You are not allowed to fetch the message. Or you are missing the permissions to do this action.
+
+        :class:`HTTPException`
+            A HTTP error occurred.
+        """
+        return await self._state.managers.messages.remove_all_reactions(
+            channel_id=self.channel_id, message_id=self.id
+        )
+
+    async def fetch_reactions(
+        self,
+        *,
+        emoji_id: int = _MISSING,
+        emoji_name: str,
+        type: ReactionType = _MISSING,
+        after: int = _MISSING,
+        limit: int = _MISSING,
+    ) -> list[User]:
+        """
+        Fetch a list of users that reacted with this emoji.
+
+        Parameters
+        ----------
+        emoji_id : :class:`int`, optional
+            The ID of the custom emoji. Omit when reacting with a unicode emoji.
+
+        emoji_name : :class:`str`
+            The unicode emoji or the name of the custom emoji.
+
+        type : :class:`ReactionType <mizuki.enums.message.ReactionType>`, optional
+            The type of the reaction to fetch. Defaults to fetches :attr:`NORMAL <mizuki.enums.message.ReactionType.NORMAL>`.
+
+        after : :class:`int`, optional
+            To fetch the list starting after the specified user ID.
+
+        limit : :class:`int`, optional
+            The max number of users to retrieve. Can be 1-100. Defaults to 25.
+
+        Raises
+        ------
+        :class:`NotFound`
+            The message you tried to fetch reactions of could not be found.
+
+        :class:`Forbidden`
+            You are not allowed to fetch the message.
+
+        :class:`HTTPException`
+            A HTTP error occurred.
+        """
+        return await self._state.managers.messages.fetch_reactions(
+            channel_id=self.channel_id,
+            message_id=self.id,
+            emoji_id=emoji_id,
+            emoji_name=emoji_name,
+            type=type,
+            after=after,
+            limit=limit,
+        )
+
+    async def edit(
+        self,
+        content: str | None = _MISSING,
+        *,
+        embeds: list[Embed] = _MISSING,
+        flags: MessageFlags = _MISSING,
+        allowed_mentions: AllowedMentions | None = _MISSING,
+        files: list[File] = _MISSING,
+        override_files: bool = True,
+    ) -> Message:
+        """
+        Edits a message sent by you.
+
+        Parameters
+        ----------
+        content : :class:`str`
+            The content of the message.
+
+        embeds : list[:class:`Embed <mizuki.objects.embed.Embed>`]
+            The embeds of the message.
+
+        flags : :class:`MessageFlags <mizuki.flags.MessageFlags>`
+            The flags of the message.
+
+        allowed_mentions : :class:`AllowedMentions <mizuki.objects.message.AllowedMentions>` | :class:`None`
+            The AllowedMentions object that dictates whether user, role or everyone pings are enabled. Pass ``None`` to set this back to default.
+
+        files : list[:class:`File <mizuki.file.File>`]
+            The files to upload to the message.
+
+        override_files : :class:`bool`
+            Whether to override or append the files to the message. Description is ignored when this is set to ``False``
+
+        Raises
+        ------
+        :class:`NotFound`
+            The message you tried to edit was not found.
+
+        :class:`Forbidden`
+            You are not allowed to edit that message.
+
+        :class:`HTTPException`
+            A HTTP error occurred.
+        """
+        return await self._state.managers.messages.edit(
+            channel_id=self.channel_id,
+            message_id=self.id,
+            content=content,
+            embeds=embeds,
+            flags=flags,
+            allowed_mentions=allowed_mentions,
+            files=files,
+            override_files=override_files,
+        )
+
+    async def delete(self) -> None:
+        """
+        Deletes a message from a channel.
+
+        This method requires :attr:`MANAGE_MESSAGES <mizuki.objects.permissions.Permissions.MANAGE_MESSAGES>` if deleting a message from someone else.
+
+        Raises
+        ------
+        :class:`NotFound`
+            The message you tried to delete does not exist.
+
+        :class:`Forbidden`
+            You are not allowed to delete that message.
+
+        :class:`HTTPException`
+            A HTTP error occurred.
+        """
+        await self._state.managers.messages.delete(
+            channel_id=self.channel_id, message_id=self.id
         )
 
 
